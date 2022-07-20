78f9bdb03eaced7c5e29f88b4356eb85ca37a6d9
Birm Wais <b.wais@outlook.com>
2022-07-20 01:44:59 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {
+  
+}
+
+const App: React.FC<Props> = (props): JSX.Element => {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
